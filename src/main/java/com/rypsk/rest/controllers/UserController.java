0c5c5009d657fc77b8408f6922b5ac592a3a8736
rypsk <rypsk1.0@gmail.com>
2021-06-28 09:26:32 +0200

    Update UserController.java

--- src/main/java/com/rypsk/rest/controllers/UserController.java
+++ src/main/java/com/rypsk/rest/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.rypsk.rest.repositories.UserRepository;
 
 @RestController
 @RequestMapping(value = "/api/user")
+//@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class UserController {
 
 	private final Logger LOG = LoggerFactory.getLogger(getClass());
